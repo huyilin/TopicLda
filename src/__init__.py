c6343ca94c05b38aa8f61cb186f5bafb7cbe1c82
Yilin Hu <Huyilin.ce@gmail.com>
2014-05-21 20:25:37 +0200

    Delete __init__.py

--- src/__init__.py
+++
@@ -1,6 +0,0 @@
-import sys
-sys.path.append("/export/home/team06/randomtrip/mysite/random_trip/src/")
-import onlineldauser
-
-vocab = file('/export/home/team06/randomtrip/mysite/random_trip/src/vocaball.txt').readlines()
-olda=onlineldauser.OnlineLDA(vocab)
